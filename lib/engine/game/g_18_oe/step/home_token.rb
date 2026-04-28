51ccb2ac4e3adfe6a3bb56b6a47c54baa872ec4c
witzman <witzman@witzman.de>
2026-04-28 08:21:15 +0200

    PR Feedback

--- lib/engine/game/g_18_oe/step/home_token.rb
+++ lib/engine/game/g_18_oe/step/home_token.rb
@@ -10,8 +10,7 @@ module Engine
           def process_place_token(action)
             if action.entity.type == :minor
               hex = action.city.hex
-              region = @game.class::CITY_NATIONAL_ZONE[hex.coordinates] ||
-                         @game.class::NATIONAL_REGION_HEXES.find { |_, hexes| hexes.include?(hex.coordinates) }&.first
+              region = @game.region_for_hex(hex)
 
               raise GameError, "Region #{region} is not available" unless @game.region_available?(region)
 
